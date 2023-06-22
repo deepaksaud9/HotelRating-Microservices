3d15d71f2165e593c53da4516614ca867fb827d3
deepak saud <deppofficial00@gmail.com>
2023-06-22 11:32:58 +0545

    resilience4j:circuit-breaker, retry,ratelimiter are used

--- UserService/src/main/java/com/user/service/controller/UserController.java
+++ UserService/src/main/java/com/user/service/controller/UserController.java
@@ -3,6 +3,11 @@ package com.user.service.controller;
 import com.user.service.entities.User;
 import com.user.service.response.ApiResponse;
 import com.user.service.service.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +19,7 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
+    private Logger LOGGER = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private UserService userService;
 
@@ -35,8 +41,8 @@ public class UserController {
 
        List<User> allUser = userService.getAllUser();
        ApiResponse<List<User>> userResponse = ApiResponse.<List<User>>builder()
-               .statusName(HttpStatus.FOUND.name())
-               .statusCode(HttpStatus.FOUND.value())
+               .statusName(HttpStatus.OK.name())
+               .statusCode(HttpStatus.OK.value())
                .success(true)
                .message("list of user found")
                .response(allUser)
@@ -45,17 +51,47 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(userResponse);
     }
 
+    int retryCount = 1;
     @GetMapping("/{userId}")
+//    @CircuitBreaker(name = "ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
+//    @Retry(name = "ratingHotelService", fallbackMethod = "ratingHotelFallback")
+    @RateLimiter(name = "userRateLimiter", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<ApiResponse> getUser(@PathVariable("userId") String userId){
+
+        LOGGER.info("Get Single User Handler : UserController");
+        LOGGER.info("RetryCount count :{}",retryCount);
+        retryCount++;
 
         User user = userService.getUserById(userId);
         ApiResponse<User> userResponse = ApiResponse.<User>builder()
-                .statusName(HttpStatus.FOUND.name())
-                .statusCode(HttpStatus.FOUND.value())
+                .statusName(HttpStatus.OK.name())
+                .statusCode(HttpStatus.OK.value())
                 .message("user Found with userId: "+userId)
                 .response(user)
                 .build();
 
-        return ResponseEntity.status(HttpStatus.FOUND).body(userResponse);
+        return ResponseEntity.status(HttpStatus.OK).body(userResponse);
+    }
+
+    //fallback method for circuit break
+
+    public ResponseEntity<ApiResponse> ratingHotelFallback(String userId, Exception ex){
+
+//        LOGGER.info("Fallback is Executed because service is down",ex.getMessage());
+        ApiResponse<User> userResponse = ApiResponse
+                .<User>builder()
+                .statusName(HttpStatus.OK.name())
+                .statusCode(HttpStatus.OK.value())
+                .message("this ia a fallback method")
+                .response(
+                         User.builder()
+                                 .userId("not found")
+                                 .name("not found")
+                                 .email("not found")
+                                 .about("not found")
+                                 .build()
+                )
+                .build();
+        return ResponseEntity.status(HttpStatus.OK).body(userResponse);
     }
 }
--- UserService/src/main/java/com/user/service/entities/User.java
+++ UserService/src/main/java/com/user/service/entities/User.java
@@ -1,9 +1,6 @@
 package com.user.service.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -17,6 +14,7 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Table(name = "users")
 public class User {
 
