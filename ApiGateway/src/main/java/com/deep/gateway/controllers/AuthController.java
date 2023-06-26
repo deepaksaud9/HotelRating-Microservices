00ae9ef2fa06e8498d87ef5b4a77088519b9b3c8
deepak saud <deppofficial00@gmail.com>
2023-06-26 13:37:38 +0545

    security started

--- ApiGateway/src/main/java/com/deep/gateway/controllers/AuthController.java
+++ ApiGateway/src/main/java/com/deep/gateway/controllers/AuthController.java
@@ -12,8 +12,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.stream.Collectors;
 
